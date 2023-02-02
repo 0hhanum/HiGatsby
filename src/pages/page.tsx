c7770c92f5d8a36bcb636907a0f81b4a99d49cec
hanum <rntls123@naver.com>
2023-02-03 00:13:07 +0900

    WOW

---
+++ src/pages/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Page() {
+  return <h1>HELLO</h1>;
+}
+export default Page;
---
+++ src/pages/test/HI.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Hi() {
+  return <h1>HI</h1>;
+}
+export default Hi;
